13732747db8786782945071ff35685c49231e470
Tiarasilvi <192010345@smkn4-tsm.sch.id>
2022-02-16 15:43:11 +0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Tutorial/>
+</template>
+
+<script>
+export default {
+  mounted(){
+    console.log(this.$supabse)
+  }
+}
+</script>
---
+++ pages/product.vue
@@ -0,0 +1,56 @@
+<template>
+<main>
+  <div class="container">
+    <div>
+      <div class="jumbotron">
+        <h2>product</h2>
+      </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div v-if="loading" class="text-center">
+              sedang memuat ...
+            </div>
+          </div>
+          <div class="col-md-4" v-for="produk in products" :key="produk.id">
+            <div class="card mb-3">
+              <div class="card-header">
+                <img :src="produk.poto" width="100%"> 
+              </div>
+              <div class="card-body">
+                <h4>{{ produk.nama }}</h4>
+                <h4>Rp{{ produk.harga }}</h4>
+                <a :href="produk.link_eksternal" class="btn btn-success btn-block">Beli</a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: '',
+      loading: true,
+    }
+  },
+  mounted() {
+    this.ambilData()
+  },
+  methods: {
+    async ambilData() {
+      const { data, error } = await this.$supabase
+        .from('tb_produk')
+        .select()
+      if(data) {
+        this.products = data
+        this.loading = false
+      }
+      if(error) console.log(error)
+    }
+  }
+}
+</script>
